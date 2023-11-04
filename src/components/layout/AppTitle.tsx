03b79b47b793f812e695d354cfa618e46c7f7fb9
RogelioRivas <apteruno@gmail.com>
2023-11-04 01:45:57 -0600

    add AppTitle component

---
+++ src/components/layout/AppTitle.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export function AppTitle() {
+  return (
+    <Typography variant='h6' color='inherit' noWrap sx={{ flexGrow: 1 }}>
+      Where in the world?
+    </Typography>
+  );
+}
